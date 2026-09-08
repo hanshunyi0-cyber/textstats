3b355c3fc61107c70b3c9b4b58730e78159bfc7b
Shunyi HAN <hanshunyi0@gmail.com>
2026-09-08 22:07:03 +0200

    Export textstats functions

--- src/textstats/__init__.py
+++ src/textstats/__init__.py
@@ -1,2 +1,3 @@
-def hello() -> str:
-    return "Hello from textstats!"
+from textstats.core import char_frequencies, longest_word, word_count
+
+__all__ = ["word_count", "char_frequencies", "longest_word"]
